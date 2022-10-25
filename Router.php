bd93d009b963f4f64b8cdd6fd909a5683f0d7c94
victor garcia <moyosxtouch@gmail.com>
2022-10-25 00:22:39 -0500

    Consultar el modelo en el controlador y pasar resultados hacia la vista

--- Router.php
+++ Router.php
@@ -24,7 +24,10 @@ if($fn) {
 }
     }
 //Muestra una vista
-public function render($view) {
+public function render($view, $datos=[]) {
+ foreach($datos as $key=>$value) {
+  $$key=$value;
+ }
   ob_start ();
 include __DIR__ . "/views/$view.php";
 $contenido=ob_get_clean();
--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -1,10 +1,13 @@
 <?php
 namespace Controllers;
 use MVC\Router;
+use Model\Propiedad;
 class  PropiedadController
 {
   public static function index(Router $router) {
-  $router->render('propiedades/admin');
+    $propiedades=Propiedad::all();
+    $resultado=null;
+  $router->render('propiedades/admin',['propiedades'=> $propiedades, 'resultado'=>$resultado]);
   }
   public static function crear() {
     echo"crear propiedad";
