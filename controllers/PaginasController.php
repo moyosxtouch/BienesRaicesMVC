202e2018cc0595a4f0609e5e7208ac073a84a73a
victor garcia <moyosxtouch@gmail.com>
2022-10-29 22:01:06 -0500

    Se hizo modificacion para enviar mails con los datos del formulario

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -31,6 +31,9 @@ class  PaginasController {
   }
     public static function contacto(Router $router) {
       if($_SERVER['REQUEST_METHOD']==='POST') {
+          // Validar 
+            $respuestas = $_POST['contacto'];
+       // debuguear($_POST);
 //Crear una instancia de PHPMailer
 $mail=new PHPMailer();
 //configurar smtp
@@ -42,18 +45,34 @@ $mail->Password='0128189faff3e5';
 $mail->SMTPSecure='tls';
 $mail->Port='2525';
 //Configurar el contenido del mail
-$mail->setFrom('admin@bienesraices.com');
+  $mail->setFrom('admin@bienesraices.com', $respuestas['nombre']);
 $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
 $mail->Subject='Tienes un nuevo mensaje';
 //Habilitar HTML
 $mail->isHTML(true);
 $mail->CharSet='UTF-8';
 //Definir el contenido
-$contenido='<html><p>Tienes un nuevo mensaje</p></html>';
-$mail->Body=$contenido;
-$mail->AltBody='Esto es texto alternativo sin HTML';
+
+            $contenido = '<html>';
+            $contenido .= "<p><strong>Has Recibido un email:</strong></p>";
+            $contenido .= "<p>Nombre: " . $respuestas['nombre'] . "</p>";
+            $contenido .= "<p>Mensaje: " . $respuestas['mensaje'] . "</p>";
+            $contenido .= "<p>Vende o Compra: " . $respuestas['opciones'] . "</p>";
+            $contenido .= "<p>Presupuesto o Precio: $" . $respuestas['presupuesto'] . "</p>";
+
+            if($respuestas['contacto'] === 'telefono') {
+                $contenido .= "<p>Eligió ser Contactado por Teléfono:</p>";
+                $contenido .= "<p>Su teléfono es: " .  $respuestas['telefono'] ." </p>";
+                $contenido .= "<p>En la Fecha y hora: " . $respuestas['fecha'] . " - " . $respuestas['hora']  . " Horas</p>";
+            } else {
+                $contenido .= "<p>Eligio ser Contactado por Email:</p>";
+                $contenido .= "<p>Su Email  es: " .  $respuestas['email'] ." </p>";
+            }
+             $contenido .= '</html>';
+            $mail->Body = $contenido;
+            $mail->AltBody = 'Esto es texto alternativo';
 //Enviar el email
-if($mail->send()) {
+ if($mail->send()){
   echo "Mensaje enviado correctamente";
 }else {
   echo"El Mensaje no se pudo enviar...";
@@ -63,3 +82,4 @@ if($mail->send()) {
     $router->render('paginas/contacto');
   }
 }
+     
--- views/paginas/contacto.php
+++ views/paginas/contacto.php
@@ -2,62 +2,65 @@
 <img src="/build/img/destacada3.jpg" alt="Imagen Principal">
 
 <main class="contenedor seccion contenido-centrado">
-    <h2 class="fw-300 centrar-texto">Llena el formulario de Contacto</h2>
+    <h2>Llene el formulario de Contacto</h2>
 
-  <form class="formulario" method="POST" action="/contacto">
+    <form class="formulario" method="POST" action="/contacto">
         <fieldset>
             <legend>Información Personal</legend>
-            <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" placeholder="Tu Nombre" name="contacto[nombre]" required>
 
+            <label for="nombre">Nombre</label>
+            <input type="text" placeholder="Tu Nombre" id="nombre" name="contacto[nombre]">
+            
             <label for="email">E-mail: </label>
-            <input type="email" id="email" placeholder="Tu Correo electrónico" name="contacto[email]" required>
+            <input type="email" id="email" placeholder="Tu Correo electrónico" required name="contacto[email]">
 
             <label for="telefono">Teléfono:</label>
-            <input type="tel" id="telefono" placeholder="Tu Teléfono" name="contacto[telefono]" >
+            <input type="tel" id="telefono" placeholder="Tu Teléfono" required name="contacto[telefono]">
 
-            <label for="mensaje">Mensaje: </label>
-            <textarea id="mensaje" name="contacto[mensaje]" required ></textarea>
-
+            <label for="mensaje">Mensaje:</label>
+            <textarea id="mensaje" name="contacto[mensaje]"></textarea>
         </fieldset>
 
-
         <fieldset>
-            <legend>Información sobre Propiedad</legend>
-            <label for="opciones">Vende o Compra</label>
-            <select id="opciones" name="contacto[tipo]" required>
+
+
+       <fieldset>
+            <legend>Información sobre la propiedad</legend>
+
+            <label for="opciones">Vende o Compra:</label>
+            <select id="opciones" name="contacto[opciones]">
                 <option value="" disabled selected>-- Seleccione --</option>
                 <option value="Compra">Compra</option>
                 <option value="Vende">Vende</option>
             </select>
-          <label for="presupuesto">Precio o Presupuesto</label>
-          <input type="number" placeholder="Tu precio o presupuesto" id="presupuesto" name="contacto[precio]" required>
+
+            <label for="presupuesto">Precio o Presupuesto</label>
+            <input type="number" placeholder="Tu Precio o Presupuesto" id="presupuesto" name="contacto[presupuesto]">
+
         </fieldset>
 
-        <fieldset>
-            <legend>Contacto</legend>
+    <fieldset>
+            <legend>Información sobre la propiedad</legend>
 
-            <p>Como desea ser Contactado:</p>
+            <p>Como desea ser contactado</p>
 
             <div class="forma-contacto">
-                <label for="telefono">Teléfono</label>
-                <input type="radio" name="contacto" value="telefono" id="telefono" name="contacto[contacto]" required>
+                <label for="contactar-telefono">Teléfono</label>
+                <input name="contacto[contacto]" type="radio" value="telefono" id="contactar-telefono" >
 
-                <label for="correo">E-mail</label>
-                <input type="radio" name="contacto" value="correo" id="correo" name="contacto[contacto]" required>
+                <label for="contactar-email">E-mail</label>
+                <input name="contacto[contacto]" type="radio" value="email" id="contactar-email" >
             </div>
-
-            <p>Si eligió Teléfono, elija la fecha y la hora</p>
+   <p>Si eligió Teléfono, elija la fecha y la hora</p>
             <label for="fecha">Fecha:</label>
-            <input type="date" id="fecha" name="contacto[fecha]">
+            <input type="date" id="fecha" name="contacto[fecha]>
 
             <label for="hora">Hora:</label>
-            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
-
+            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]>
+            <div id="contacto"></div>
 
         </fieldset>
 
-        <input type="submit" value="Enviar" class="boton boton-verde">
-
+        <input type="submit" value="Enviar" class="boton-verde">
     </form>
 </main>
